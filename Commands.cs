e68b96c6685978407c78c914f5d3e509f3d4f5c0
omeralitaylan <omeralitaylan@outlook.com>
2025-01-14 23:19:27 +0300

    message update

--- Commands.cs
+++ Commands.cs
@@ -1,6 +1,7 @@
 /*
- * I started the project with the code build you provided. After adding some features to the game,
- * I started getting some errors and I used AI to solve them. (Claude Ai)
+ I asked you a question about the use of AI in the classroom. Before asking that question, I had made another much more detailed game with AI and another code structure, but after I asked you the question, I changed my mind and decided to cancel this game and create something with your structure.
+
+I built the project on the code structure you gave. After some features I added to the game, I encountered errors and edited the project with Claude AI to fix these errors.
  */
 
 namespace IslandAdventure;
